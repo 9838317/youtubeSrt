6271e54a2d01c2db5f166d78709669073aefe3f8
9838317@qq.com <9838317@qq.com>
2017-05-31 09:23:59 +0800

    automatic commit

---
+++ rungit.sh
@@ -0,0 +1,9 @@
+git config --global user.name "9838317@qq.com"
+git config --global user.email 9838317@qq.com
+git  config --global alias.st status
+git  config --global alias.a add
+git  config --global alias.b branch
+git  config --global alias.c checkout
+git  config --global alias.cm "commit -m 'automatic commit'"
+git  config --global credential.helper cache
+git  config --global credential.helper 'cache --timeout=7200'
